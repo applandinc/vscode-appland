0604c8e84292acfae7cd1c3f589f8cfc1bfc3027
Ty Paulhus <typaulhus@gmail.com>
2021-01-08 17:07:43 -0500

    initial content

--- src/scenarioViewer.ts
+++ src/scenarioViewer.ts
@@ -174,11 +174,42 @@ export class ScenarioProvider implements vscode.CustomTextEditorProvider {
 			<body>
 				<div id="appland">
 					<div class="sidebar">
+
+						<div id="intro-details" class="welcome">
+							<h3 class="block-heading">Welcome to AppLand!</h3>
+							<div class="content intro">
+								<p>Select a component in the diagram to see its details here. Here, we’ll show you any relevant information about your code as you navigate through your appmaps.</p>
+								<p>Check the extension page for full details on what is possible with AppLand.</p>
+								<div class="components_events">
+									<h3>Components & Events</h3>
+								<p>The UML-inspired diagrams display your key application components, and how they are inter-related during application execution.</p>
+								</div>
+								<div class="right_click">
+									<h3>Right-click</h3>
+									<p>Right-click nodes in the diagram to expand, collapse, and reset the diagram elements.</p>
+								</div>
+								<div class="filters">
+									<h3>Filters</h3>
+									<p>Use our powerful filters bring focus to a narrow area of youyr interest.</p>
+								</div>
+								<div class="legend">
+									<ul>
+										<li>Class packages. Right-click to expand them to classes.</li>
+										<li>Class. Right-click to see source (Ruby only)</li>
+										<li>Dynamic dependencies</li>
+										<li>Web services/endpoints. Right-click to expand.</li>
+										<li>Databases that persist application data</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+
 						<div id="component-details" class="selection-details">
 							<h3 class="block-heading">Component details</h3>
 							<div class="content">
 							</div>
 						</div>
+
 					</div>
 					<div id="diagram">
 						<div id="errors"></div>
